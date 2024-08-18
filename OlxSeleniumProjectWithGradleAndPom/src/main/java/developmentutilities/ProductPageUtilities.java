e892d9fc9aeaf256eb6481352d4ada5d2bd67d1c
fandinho1 <ldfandiv@gmail.com>
2024-08-17 23:28:18 -0500

    Removing old project due are not updated with new versions

--- OlxSeleniumProjectWithGradleAndPom/src/main/java/developmentutilities/ProductPageUtilities.java
+++
@@ -1,8 +0,0 @@
-package developmentutilities;
-
-import org.openqa.selenium.By;
-
-public class ProductPageUtilities {
-	public static final By TITTLE_PRODUCT = By.xpath("//div[@class='title']/h1");
-
-}
--- SeleniumExcelGradleWithPom/src/main/java/developmentutilities/ExcelOperationsUtilities.java
+++
@@ -1,7 +0,0 @@
-package developmentutilities;
-
-public class ExcelOperationsUtilities {
-	
-	public static final String RUTA_EXCEL = "C:\\Users\\SEMILLERO02\\Desktop\\documentos\\datos.xlsx";
-
-}
